b967ba39f05c979e1d861cc2451ac2e5d1fd1eeb
bendikMichal <mbendik97@gmail.com>
2024-06-17 19:09:53 +0200

    user.js testing

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
 
+const clock_update_seconds = 10;
+
 const find = () => {
 	console.log(searchbar);
 	let search_value = searchbar.value;
 	window.location.href = `https://duckduckgo.com/?q=${encodeURI(search_value)}`;
 }
 
+const set_clock = () => {
+	const str = formatter.format(new Date());
+	clock.textContent = str;
+}
+
 let searchbar = document.getElementsByClassName("searchbar")[0];
-let search_form = document.getElementById("searchform");
 let search_button = document.getElementsByClassName("glass")[0];
-search_form.onsubmit= find;
+let clock = document.getElementsByClassName("clock")[0];
+
+searchbar.addEventListener('keydown', event => {
+  if (event.key === 'Enter' || event.keyCode === 13) find()
+});
 search_button.onclick = find;
 
-console.log(search_button);
+const formatter = new Intl.DateTimeFormat('en-US', { hour: '2-digit', minute: '2-digit' });
+set_clock();
+let clock_interval = setInterval(set_clock, clock_update_seconds * 1000);
 
 
