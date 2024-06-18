e05a45a61c5e7cf633cb3b0f1d3ca623a5bd2182
bendikMichal <mbendik97@gmail.com>
2024-06-18 18:44:13 +0200

    setting preview

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 
 const clock_update_seconds = 10;
+const image_url = "https://picsum.photos/2048";
 
 const find = () => {
 	console.log(searchbar);
@@ -12,6 +13,14 @@ const set_clock = () => {
 	clock.textContent = str;
 }
 
+let settings_open = false;
+const open_settings = () => {
+	settings_open = !settings_open;
+	settings_menu.style.display = settings_open ? "flex" : "none";
+}
+const close_settings = () => settings_open = true && open_settings();
+
+
 let searchbar = document.getElementsByClassName("searchbar")[0];
 let search_button = document.getElementsByClassName("glass")[0];
 let clock = document.getElementsByClassName("clock")[0];
@@ -21,7 +30,15 @@ searchbar.addEventListener('keydown', event => {
 });
 search_button.onclick = find;
 
-document.body.style.backgroundImage = "url(\"https://picsum.photos/2048\")" 
+let settings_menu = document.getElementsByClassName("settings-menu")[0];
+let settings_button = document.getElementsByClassName("settings")[0];
+settings_button.onclick = open_settings;
+
+
+
+document.body.style.backgroundImage = `url("${image_url}")`
+settings_menu.onblur = close_settings;
+
 const formatter = new Intl.DateTimeFormat('en-US', { hour: '2-digit', minute: '2-digit' });
 set_clock();
 let clock_interval = setInterval(set_clock, clock_update_seconds * 1000);
